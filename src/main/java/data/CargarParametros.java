978a37a35f28c88beb6ddf86064e0b483a787986
Bonansea Mariano <marianobonanseapetrovial@gmail.com>
2024-09-27 19:15:07 -0300

    Carga de datos de Archivo sin terminar

---
+++ src/main/java/data/CargarParametros.java
@@ -0,0 +1,44 @@
+package data;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class CargarParametros {
+
+    private static String archivoComputadora;
+    private static String archivoRouter;
+    private static String archivoConexion;
+
+    /**
+     * Loads the properties from the "config.properties" file and sets the values
+     * of the static variables `archivoComputadora`, `archivoRouter`, and
+     * `archivoConexion` based on the corresponding property keys.
+     *
+     * @throws IOException if an I/O error occurs while reading the properties file
+     */
+    public static void parametros() throws IOException {
+        Properties prop = new Properties();
+        InputStream input = new FileInputStream("config.properties");
+        // load a properties file
+        prop.load(input);
+        // get the property value
+        archivoComputadora = prop.getProperty("computadora");
+        archivoRouter = prop.getProperty("router");
+        archivoConexion = prop.getProperty("conexion");
+    }
+
+    public static String getArchivoComputadora() {
+        return archivoComputadora;
+    }
+
+    public static String getArchivoRouter() {
+        return archivoRouter;
+    }
+
+    public static String getArchivoConexion() {
+        return archivoConexion;
+    }
+
+}
