899be8b7be7f85df00ac31a787d14aa99010bf5a
Bonansea Mariano <marianobonanseapetrovial@gmail.com>
2024-10-28 17:20:32 -0300

    Fix Ips

--- src/main/java/gui/PingRangeDialog.java
+++ src/main/java/gui/PingRangeDialog.java
@@ -144,11 +144,10 @@ public class PingRangeDialog extends javax.swing.JDialog {
                     ipsCollection.add(ip);
                 }
             }
-        }else{
-            ipsCollection.add(ip1);
+
+            ipsCollection.add(ip2);
         }
 
-        ipsCollection.add(ip2);
         for(Map.Entry   <String, Boolean> entry : coordinator.pingRange(ipsCollection).entrySet()) {
             pingRangeText += "\n" + entry.getKey() + ": " + (entry.getValue() ? "Conectado" : "Desconectado");
         }
